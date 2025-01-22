8e22ac41476ce12b42ca05ec250f51822d099009
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2025-01-22 10:29:24 +0530

    feat: template for websocket go

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+  "log"
+  "net/http"
+  "github.com/gorilla/websocket"
+)
+
+type webSocketHandler struct {
+  upgrader websocket.Upgrader
+}
+
+func (wsh webSocketHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  conn, err := wsh.upgrader.Upgrade(w, r, nil)
+  if err != nil {
+    log.Println("Error %s when connecting to the socket", err)
+    return 
+  }
+
+  for{
+    mt, message, err := conn.ReadMessage()
+    if err != nil {
+      log.Println("Error %s when reading the message", err)
+    }
+
+    if mt == websocket.Binarymessage {
+      err = c.WriteMessage(websocket.TextMessage, []byte("server doesn't support binary messages"))
+      if err != nil {
+        log.Printf("Error %s when sending message to client", err)
+      }
+      return
+    }
+
+    
+  }
+
+  defer conn.Close()
+}
+
+func main(){
+  wsh := webSocketHandler{
+    upgrader: websocket.Upgrader{},
+  }
+
+  http.Handle("/ws", wsh)
+  log.Println("Server starting")
+  log.Fatal(http.ListenAndServe(":8080", nil))
+  
+}
